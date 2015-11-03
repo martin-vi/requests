da73308da0cd3877295ca006ccb0ccfab23f7979
Martin Vieweg <martin.vieweg@scale.eu>
2015-11-03 09:40:33 +0100

    2.8.1+scale1

--- requests/__init__.py
+++ requests/__init__.py
@@ -42,7 +42,7 @@ is at <http://python-requests.org>.
 """
 
 __title__ = 'requests'
-__version__ = '2.8.1'
+__version__ = '2.8.1+scale1'
 __build__ = 0x020801
 __author__ = 'Kenneth Reitz'
 __license__ = 'Apache 2.0'
